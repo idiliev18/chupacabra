998f47ca371f08ffe7394190bdae9472f4b283e9
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-06-23 19:37:24 +0300

    Add information to Home page

--- client/src/pages/Index.jsx
+++ client/src/pages/Index.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 function Index(props) {
     return (
         <>
-            <Hero size="medium" color="link">
+            <Hero size="smal" color="link">
                 <Hero.Body>
                     <div className="title">
                         Намерете новини за пристанища и морето
@@ -17,27 +17,25 @@ function Index(props) {
                     </div>
                 </Hero.Body>
             </Hero>
-            <Hero size="medium" color="warning">
+            <Hero size="medium" color="link">
                 <Hero.Body>
-                    <div className="title">lorem ipsum</div>
-                    <div className="subtitle">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Molestias omnis dolor in, blanditiis doloremque ullam
-                        tenetur soluta ipsum? Nulla iste modi saepe facere
-                        impedit explicabo numquam minima inventore, dolor
-                        necessitatibus?
+                    <div className="title info-module">За сайта</div>
+                    <div className="subtitle info-module">
+                        Нашият уебсайт има за цел да улесни работата на пристанищата, максимално автоматизирайки ги. Освен за пристанища
+                        той е подходящ за рибари, независимо професионално или под формата на хоби, защото предоставя информация за рибите,
+                        пасажите им и разбира се актуална информация за риболова. Уебсайтът има и секция където рибарите могат да продават
+                        рибата, която са хванали и така да се подпомогне за тяхното разпродаване. Освен информативен сайтът цели и да бъде
+                        място, където рибарите да качат снимки на улова си, за да се похвалят или да използват платформата за комуникация 
+                        с останалите рибари. За някои от тези функции е нужна регистрация, а за да си направите такава натиснете  
+                        <Link to="/Signup"> тук</Link>.
                     </div>
                 </Hero.Body>
             </Hero>
-            <Hero size="medium" color="danger">
+            <Hero size="medium is-success">
                 <Hero.Body>
-                    <div className="title">lorem ipsum</div>
+                    <div className="title">Актуално</div>
                     <div className="subtitle">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Molestias omnis dolor in, blanditiis doloremque ullam
-                        tenetur soluta ipsum? Nulla iste modi saepe facere
-                        impedit explicabo numquam minima inventore, dolor
-                        necessitatibus?
+                        News section
                     </div>
                 </Hero.Body>
             </Hero>
