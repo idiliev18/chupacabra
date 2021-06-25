8d7383b817cbe0a4df40a460091a155673cff4ce
idiliev18 <idiliev18@codingburgas.bg>
2021-06-25 23:42:52 +0300

    Test CodeQL action

--- server/routes/api.js
+++ server/routes/api.js
@@ -20,6 +20,11 @@ app.post('/login', async (req, res) => {
     let returnValue = true;
     let resJSON;
 
+    let password = 123233;
+    let securedPassword = (password+"fikretchu");
+
+
+
     loggerManager.logInfo(
         `User with email: ${loginData.email} is trying to login.`
     );
