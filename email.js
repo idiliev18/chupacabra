169da618cdb8adbe782dad7ae54b9cb31be2ab75
KNTaligadzhiev19 <KNTaligadzhiev19@codingburgas.bg>
2021-06-27 14:31:44 +0300

    Modify verification email

--- email.js
+++ email.js
@@ -12,24 +12,24 @@ async function main() {
         host: 'smtp.ethereal.email',
         port: 587,
         auth: {
-            user: 'carmelo71@ethereal.email',
-            pass: 'J6YyHAANjdMYZDNZJ7'
+            user: 'ozella.skiles@ethereal.email',
+            pass: 'VReq3xhBu1E5q2h9Pg'
         }
     });
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
         from: 'foo@email.cb', // sender address
-        to: "foo@email.cb", // list of receivers
+        to: "ozella.skiles@ethereal.email", // list of receivers
         subject: "Потвърждение на имайл адрес", // Subject line
-        text: "Здравейте 👋! Радваме се, че се регистрирахте ❤! За да използвате пълните функционалности на нашата платформа моля потвърдете Вашия имайл, натискайки бутона ⬇.", // plain text body
         html: `
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;700&family=Ubuntu:wght@300&display=swap" rel="stylesheet">
         <h1 style="margin-left: 5%;">Здравейте 👋!</h1> <br> 
-        <p style="font-size: 20px; margin-left: 10%; margin-right: 10%;">Радваме се, че се регистрирахте &#128151! За да използвате пълните функционалности на нашата платформа 
-        моля потвърдете Вашия имайл, натискайки бутона 🢃.</p> <br> <br> <br> 
+        <p style="font-size: 20px; margin-left: 10%; margin-right: 10%;">Радваме се, че се регистрирахте &#128151 на нашият сайт! За да използвате пълните функционалности на нашата платформа, 
+        моля потвърдете Вашия имейл, натискайки синия бутона долу 🢃.</p> <br>
+        <img src="https://cdn.discordapp.com/attachments/834872582661603440/858667540648951839/dancing_fish.gif" height="400px" style="margin-left:32%;"><br>
         <a href="https://codingburgas.org"><button style="margin-left: 40%; height:50px; width:20%; background-color: #12b0df; color:white; border-radius:200px; border: none; cursor: pointer; font-size: 20px;">Потвърждаване</button></a>
         <br><br><h2 style="margin-left: 10%;">Благодарим!</h4> <br><br>
         <a href="codingburgas.bg" style="text-decoration: none; font-size: 15px; color: #0b7dbd;"><p>Обратно в сайта ⮌    <span>🔗</span> </p></a> 
@@ -49,9 +49,10 @@ async function main() {
         }
         console.log('Email sent: ' + info.response);
     });
+    sendMail();
 }
 
-//sendMail();
+
 
 
 
