9e94a91e14b884fd6017b903833eed9f19e83438
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2021-06-27 08:03:19 +0300

    Change Api class

--- code-testing/api.py
+++ code-testing/api.py
@@ -1,19 +1,8 @@
-import requests
-
 class Api:
-    def __init__(self):
-        self._ulrWithoutRoute = "http://localhost:3000/api/login"
-        self._route = None
-        self._url = None
-
-    @property
-    def url(self):
-        return self._url
-
-    @url.setter
-    def url(self, route):
-        self._url = self._ulrWithoutRoute + route
+    def __init__(self, route):
+        self._urlWithoutRoute = "http://localhost:4000"
+        self._url = self._urlWithoutRoute + route
 
 
-if __name__ == "__main__":
-    Api = Api()
+
+
